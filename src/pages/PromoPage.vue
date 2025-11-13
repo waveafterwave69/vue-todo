3a6dc981b852ea2e02098558f04c85ae5e036ca2
Michael Shonov <waveafterwave3w2@gmail.com>
2025-11-13 22:48:40 +0300

    feat: promo page

---
+++ src/pages/PromoPage.vue
@@ -0,0 +1,184 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <section class="promo">
+        <div class="promo__content">
+            <div class="promo__row">
+                <h1 class="promo__title">Управление задачами & To-Do List</h1>
+                <p class="promo__text">
+                    Этот продуктивный инструмент создан, чтобы помочь ты лучше
+                    справишься со своей задачей проектно удобно!
+                </p>
+                <RouterLink to="/home" class="promo__button">
+                    <span>Давайте начнем</span>
+                    <img src="../assets/arrow-left.svg" alt="arrow-up" />
+                </RouterLink>
+            </div>
+            <img
+                src="../assets/main-img.png"
+                alt="todo-img"
+                class="promo__img"
+            />
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.promo__content {
+    margin-top: 100px;
+    display: flex;
+    align-items: center;
+    column-gap: 100px;
+    justify-content: center;
+}
+
+.promo__img {
+    width: 500px;
+}
+
+.promo__title {
+    font-weight: 600;
+    font-size: 44px;
+    max-width: 550px;
+    margin-bottom: 35px;
+}
+
+.promo__text {
+    font-weight: 400;
+    font-size: 20px;
+    color: var(--color-grey);
+    max-width: 550px;
+    margin-bottom: 35px;
+}
+
+.promo__button {
+    width: 260px;
+    display: flex;
+    align-items: center;
+    column-gap: 20px;
+    font-weight: 600;
+    font-size: 19px;
+    text-align: center;
+    color: var(--color-white);
+    background-color: var(--color-purple);
+    padding: 15px 0px;
+    justify-content: center;
+    border-radius: 10px;
+}
+
+.promo__button:hover {
+    background-color: #502bbe;
+}
+
+.promo__button:active {
+    position: relative;
+    top: 1px;
+}
+
+@media (max-width: 1024px) {
+    .promo__content {
+        margin-top: 50px;
+        column-gap: 30px;
+    }
+
+    .promo__img {
+        width: 400px;
+    }
+
+    .promo__title {
+        font-size: 32px;
+        max-width: 350px;
+        margin-bottom: 25px;
+    }
+
+    .promo__text {
+        font-size: 18px;
+        color: var(--color-grey);
+        max-width: 350px;
+        margin-bottom: 25px;
+    }
+
+    .promo__button {
+        width: 220px;
+        column-gap: 10px;
+        font-size: 18px;
+        padding: 15px 0px;
+    }
+}
+
+@media (max-width: 768px) {
+    .promo__row {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .promo__content {
+        margin-top: 50px;
+        column-gap: 30px;
+        flex-direction: column;
+        row-gap: 20px;
+    }
+
+    .promo__img {
+        width: 300px;
+    }
+
+    .promo__title {
+        font-size: 30px;
+        max-width: 350px;
+        margin-bottom: 25px;
+        text-align: center;
+    }
+
+    .promo__text {
+        font-size: 16px;
+        color: var(--color-grey);
+        max-width: 350px;
+        margin-bottom: 25px;
+        text-align: center;
+    }
+
+    .promo__button {
+        width: 220px;
+        column-gap: 10px;
+        font-size: 16px;
+        padding: 11px 0px;
+    }
+}
+
+@media (max-width: 425px) {
+    .promo__content {
+        margin-top: 50px;
+        row-gap: 20px;
+    }
+
+    .promo__img {
+        width: 250px;
+    }
+
+    .promo__title {
+        font-size: 26px;
+        max-width: 300px;
+        margin-bottom: 25px;
+        text-align: center;
+    }
+
+    .promo__text {
+        font-size: 16px;
+        color: var(--color-grey);
+        max-width: 300px;
+        margin-bottom: 25px;
+        text-align: center;
+    }
+
+    .promo__button {
+        width: 220px;
+        column-gap: 10px;
+        font-size: 16px;
+        padding: 11px 0px;
+    }
+}
+</style>
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,8 @@
 import HomePage from '@/pages/HomePage.vue'
+import PromoPage from '@/pages/PromoPage.vue'
 import { createMemoryHistory, createRouter } from 'vue-router'
 
-const routes = [{ path: '/', component: HomePage }]
+const routes = [{ path: '/', component: PromoPage }, { path: '/home', component: HomePage }]
 
 export const router = createRouter({
     history: createMemoryHistory(),
